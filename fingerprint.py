14743ecf9ee63dbaba41f7628ac2f2f0694b56d1
sarayu19724 <madigundusarayu@gmail.com>
2026-06-21 12:26:33 +0530

    .

--- fingerprint.py
+++ fingerprint.py
@@ -9,7 +9,7 @@ from scipy.ndimage import maximum_filter
 # CONFIG
 # =====================================================
 
-SR = 22050 # Sample rate
+SR = 11025 # Sample rate
 N_FFT = 2048  # FFT window size
 HOP_LENGTH = 512  # Hop length for STFT
 
